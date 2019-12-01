b9afbe5e1350c444f874e32779975cd6e7975658
Deep-11 <58380807+Deep-11@users.noreply.github.com>
2019-12-01 13:12:17 +0530

    second commit

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.get('/', function(req, res){
 	//res.send("welcome to the first node program");
 	res.render('index');
 });
-const PORT = 3000;
-const IP = '127.0.0.1';
+const PORT = process.env.PORT;
+const IP = process.env.IP;
 
 app.listen(PORT, IP, function(){
 	console.log("node server is up and running");
